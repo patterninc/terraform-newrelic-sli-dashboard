a53b068dae7b635a3820e1c1fe75921d5d333352
juilichormole <juili.chormole@pattern.com>
2023-05-02 14:07:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,18 +9,10 @@ resource "newrelic_one_dashboard" "dashboard" {
   permissions = "public_read_only"
   page {
     name = "${local.application_name} - ( ${local.env_name} )"
-    widget_markdown {
-      title  = "Dashboard Name"
-      row    = 1
-      column = 1
-      height = 2
-      width  = 4
-      text   = "# **${var.dashboard_name} - ${local.env_name}**\n"
-    }
     widget_billboard {
       title    = "Availability"
       row      = 1
-      column   = 5
+      column   = 1
       height   = 2
       width    = 4
       warning  = 98
@@ -32,8 +24,8 @@ resource "newrelic_one_dashboard" "dashboard" {
     }
     widget_billboard {
       title    = "Application Apdex"
-      row      = 1
-      column   = 9
+      row      = 3
+      column   = 1
       height   = 2
       width    = 4
       warning  = 0.90
@@ -45,9 +37,9 @@ resource "newrelic_one_dashboard" "dashboard" {
     }
     widget_line {
       title  = "Web Error Rate"
-      row    = 3
-      column = 1
-      height = 3
+      row    = 1
+      column = 5
+      height = 2
       width  = 4
       nrql_query {
         query = "SELECT count(apm.service.error.count) / count(apm.service.transaction.duration) as 'Web errors' FROM Metric WHERE (appName = '${local.application_name}') AND (transactionType = 'Web') SINCE ${var.query_time_range} ago TIMESERIES"
@@ -55,9 +47,9 @@ resource "newrelic_one_dashboard" "dashboard" {
     }
     widget_line {
       title  = "Non-Web Error Rate"
-      row    = 3
-      column = 5
-      height = 3
+      row    = 1
+      column = 9
+      height = 2
       width  = 4
       nrql_query {
         query = "SELECT count(apm.service.error.count) / count(apm.service.transaction.duration) as 'Non-Web errors' FROM Metric WHERE (appName = '${local.application_name}') AND (transactionType = 'Other') SINCE ${var.query_time_range} ago TIMESERIES"
@@ -66,8 +58,8 @@ resource "newrelic_one_dashboard" "dashboard" {
     widget_line {
       title  = "Latency"
       row    = 3
-      column = 9
-      height = 3
+      column = 5
+      height = 2
       width  = 4
       nrql_query {
         query = "SELECT percentage(count(*), where duration <= .30) FROM Transaction WHERE (appName = '${local.application_name}') SINCE ${var.query_time_range} ago TIMESERIES"
