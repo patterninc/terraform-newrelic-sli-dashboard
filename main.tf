ccff83a9150a756de77d41e648981e8d7aee9a30
juilichormole <juili.chormole@pattern.com>
2023-05-02 12:43:18 +0530

    modified queries and widget dimensions

--- main.tf
+++ main.tf
@@ -13,59 +13,49 @@ resource "newrelic_one_dashboard" "dashboard" {
       title  = "Dashboard Name"
       row    = 1
       column = 1
-      height = 1
-      width  = 2
-      text   = "# **${var.dashboard_name} - ${local.env_name}**\n"
+      height = 3
+      width  = 1
+      text   = "# **${var.dashboard_name}-${local.env_name}**\n"
     }
     widget_billboard {
       title    = "Application Apdex"
-      row      = 1
+      row      = 2
       column   = 1
       height   = 2
-      width    = 2
+      width    = 3
       warning  = 0.90
       critical = 0.89
 
       nrql_query {
-        query = "SELECT apdex(duration, t: 0.5) FROM Transaction WHERE appName = (appName = '${local.application_name}') AND (label.Environment = '${local.env_name}') AND name != 'Controller/admin/status' SINCE ${var.query_time_range} ago"
+        query = "SELECT apdex(duration, t: 0.5) FROM Transaction WHERE (appName = '${local.application_name}')  AND name != 'Controller/admin/status' SINCE ${var.query_time_range} ago"
       }
     }
     widget_line {
       title  = "Web Error Rate"
       row    = 1
-      column = 3
-      height = 3
-      width  = 3
-      nrql_query {
-        query = "SELECT count(apm.service.error.count) / count(apm.service.transaction.duration) as 'Web errors' FROM Metric WHERE (appName = '${local.application_name}') AND (label.Environment = '${local.env_name}') AND (transactionType = 'Web') SINCE ${var.query_time_range} ago TIMESERIES"
-      }
-    }
-    widget_line {
-      title  = "Non-Web Error Rate"
-      row    = 1
-      column = 6
-      height = 3
-      width  = 3
-      nrql_query {
-        query = "SELECT count(apm.service.error.count) / count(apm.service.transaction.duration) as 'Non-Web errors' FROM Metric WHERE (appName = '${local.application_name}') AND (label.Environment = '${local.env_name}') AND (transactionType = 'Other') SINCE ${var.query_time_range} ago TIMESERIES"
-      }
-    }
-    widget_line {
-      title  = "Non-Web Error Rate"
-      row    = 1
-      column = 5
+      column = 4
       height = 3
       width  = 4
       nrql_query {
-        query = "SELECT count(apm.service.error.count) / count(apm.service.transaction.duration) as 'Web errors' FROM Metric WHERE (appName = '${local.application_name}') AND (label.Environment = '${local.env_name}') AND (transactionType = 'Web') SINCE ${var.query_time_range} ago TIMESERIES"
+        query = "SELECT count(apm.service.error.count) / count(apm.service.transaction.duration) as 'Web errors' FROM Metric WHERE (appName = '${local.application_name}') AND (transactionType = 'Web') SINCE ${var.query_time_range} ago TIMESERIES"
+      }
+    }
+    widget_line {
+      title  = "Non-Web Error Rate"
+      row    = 1
+      column = 8
+      height = 3
+      width  = 4
+      nrql_query {
+        query = "SELECT count(apm.service.error.count) / count(apm.service.transaction.duration) as 'Non-Web errors' FROM Metric WHERE (appName = '${local.application_name}') AND (transactionType = 'Other') SINCE ${var.query_time_range} ago TIMESERIES"
       }
     }
     widget_billboard {
       title    = "Availability"
-      row      = 2
+      row      = 4
       column   = 1
       height   = 2
-      width    = 2
+      width    = 3
       warning  = 98
       critical = 99
 
@@ -75,12 +65,12 @@ resource "newrelic_one_dashboard" "dashboard" {
     }
     widget_line {
       title  = "Latency"
-      row    = 2
-      column = 3
-      height = 3
+      row    = 4
+      column = 4
+      height = 4
       width  = 3
       nrql_query {
-        query = "SELECT percentage(count(*), where duration <= .30) FROM Transaction WHERE (appName = '${local.application_name}') AND (label.Environment = '${local.env_name}') SINCE ${var.query_time_range} ago TIMESERIES"
+        query = "SELECT percentage(count(*), where duration <= .30) FROM Transaction WHERE (appName = '${local.application_name}') SINCE ${var.query_time_range} ago TIMESERIES"
       }
     }
   }
